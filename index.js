21f1e8d67fe4093d52a457859e2a52923aaa6840
Einhander <supermario3ebros@gmail.com>
2023-12-08 23:37:55 -0600

    Doing some tests with the database connection

--- index.js
+++ index.js
@@ -4,6 +4,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {readFile} = require('fs').promises;
+// Conexión a la BD
+const mysql = require('mysql2');
+var config = {
+    host:"whisper-escom-server.mysql.database.azure.com",
+    user:"twmopiafgh",
+    password:"Whisper-ESCOM",
+    database:"whisper-escom-database",
+    port:3306,
+    ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
+}
+
+const conn = new mysql.createConnection(config);
+conn.connect(
+    function(err){
+        if(err){
+            console.log("!!! Cannot connect !!! Error:");
+            throw err;
+        }
+        else{
+            console.log("Connection established.");
+            readData();
+        }
+    });
+
+
+// Function to retrieve all rows from users table
+function readData(){
+    conn.query("SELECT * FROM users",function(err,results,fields){
+        if(err) throw err;
+        else console.log("Selected "+results.length+" rows.");
+        for(i=0;i<results.length;i++){
+            console.log("Row: "+JSON.stringify(results[i]));
+        }
+        console.log("Done.");
+    });
+}
 
 // Creamos la aplicación express
 const app = express();
@@ -25,23 +61,37 @@ app.get('/', async (req, res) => {
 	}
 });
 
-app.get('/usuarios', (req, res) => {
-    res.send('Aqui se listaran todos los usuarios');
+app.post('/', (req, res) => {
+    let user = req.body.user;
+    let pass = req.body.password;
+    conn.query(`INSERT INTO users(username, password) VALUES('${user}','${pass}')`);
 });
 
-app.get('/usuarios/:id', (req, res) => {
-    res.send('Aqui se listaran el usuario con id: ' + req.params.id);
+app.get('/usuarios', (req, res) => {
+    readData();
 });
 
 app.post('/usuarios', (req, res) => {
-    res.send('Aqui se creara un nuevo usuario');
+    // Creating a simple user table with username and password
+    conn.query("CREATE TABLE IF NOT EXISTS users("+
+    "id INT NOT NULL AUTO_INCREMENT,"+
+    "PRIMARY KEY(id),"+
+    "username VARCHAR(30),"+
+    "password VARCHAR(30)"+
+    ")"
+);
+
 });
 
-app.put('/usuarios/:id', (req, res) => {
-    res.send('Aqui se actualizara el usuario con id: ' + req.params.id);
+app.put('/usuarios', (req, res) => {
+    
+    // Adding 2 records
+    conn.query("INSERT INTO users(username, password) VALUES('user1','pass1')");
+    conn.query("INSERT INTO users(username, password) VALUES('user2','pass2')");
+
 });
 
-app.delete('/usuarios/:id', (req, res) => {
+app.delete('/usuarios', (req, res) => {
     res.send('Aqui se eliminara el usuario con id: ' + req.params.id);
 });
 
